f8577c7838883bb3fc43ee108c7df365d0e586f7
Dana Lu <dlu200774@gmail.com>
2021-10-24 10:10:33 -0400

    login & splash screen

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button} from 'react-native';
 import Map from "./Map"
 import { NavigationContainer } from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import LoginScreen from './Login';
 
 function LoginPage({navigation}) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button
-        title="Go to Main App"
-        onPress={() => navigation.navigate('MainApp')}
-      />
-    </View>
-  );
+  return LoginScreen(navigation);
 }
 
 function MainScreen() {
@@ -38,12 +30,3 @@ export default class App extends React.Component {
     )
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
