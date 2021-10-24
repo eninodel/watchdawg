a70a4fd301ca09a071457226cd1d6cc513681eb6
Dana Lu <dlu200774@gmail.com>
2021-10-24 09:24:50 -0400

    basic navigation

--- App.js
+++ App.js
@@ -1,14 +1,41 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
 import Map from "./Map"
+import { NavigationContainer } from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+function LoginPage({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to Main App"
+        onPress={() => navigation.navigate('MainApp')}
+      />
+    </View>
+  );
+}
+
+function MainScreen() {
+  return (
+    <Map />
+  );
+}
+
+const stack = createNativeStackNavigator();
 
 export default class App extends React.Component {
   render() {
     return (
-      <>
-    <Map />
-      </>
-    );
+      <NavigationContainer>
+        <stack.Navigator initialRouteName='Login' screenOptions={{
+          headerShown: false
+        }}>
+          <stack.Screen name="Login" component={LoginPage}/>
+          <stack.Screen name="MainApp" component={MainScreen} />
+        </stack.Navigator>
+      </NavigationContainer>
+    )
   }
 }
 
